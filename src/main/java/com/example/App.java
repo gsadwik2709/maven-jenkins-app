070cd74ccf2edca07370504c6633fb306647cfa1
Vignesh-5002 <vigneshdama2010@gmail.com>
2026-04-02 14:36:32 +0530

    second commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins CI/CD!");
+        System.out.println("hello from maven")
     }
 }
