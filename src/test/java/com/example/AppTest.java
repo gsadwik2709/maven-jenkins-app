9780df888d31a0b3f5bb6fd522de47f5f7a70922
Vignesh-5002 <vigneshdama2010@gmail.com>
2026-04-02 14:30:05 +0530

    inital commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest{
+
+    @Test
+    public void testApp() {
+        // Simple test case
+        assertTrue(true);
+    }
+}
